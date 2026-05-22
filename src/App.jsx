df0f6677f063873ac66b9e313b8f1f67d6b16b63
kwonsunwook <kwonsunwook25@gmail.com>
2026-05-22 18:21:40 +0900

    fix: App.jsx 관제탑 파일에 수리 완료된 FieldManagerApp 인터페이스 정식 연결 마감

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import FieldManagerApp from './FieldManagerApp'
+import React from 'react';
+import FieldManagerApp from './FieldManagerApp';
 
 function App() {
   return (
-    <div className="bg-slate-50 min-h-screen">
-      <FieldManagerApp />
-    </div>
-  )
+    <FieldManagerApp />
+  );
 }
-export default App
+
+export default App;
